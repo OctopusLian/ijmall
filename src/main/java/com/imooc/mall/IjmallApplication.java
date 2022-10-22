ff261e9736e94addf4087feb8321eb450cf24d11
OctopusLian <zoctopus@qq.com>
2022-10-22 22:47:16 +0800

    Initial commit

---
+++ src/main/java/com/imooc/mall/IjmallApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjmallApplication.class, args);
+	}
+
+}
