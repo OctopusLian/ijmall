7675d7d7e103bd4205f3e9726b68d5ece73e13ed
OctopusLian <zoctopus@qq.com>
2022-10-26 23:08:48 +0800

    add common and exception

--- src/main/java/com/imooc/mall/IjmallApplication.java
+++ src/main/java/com/imooc/mall/IjmallApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IjmallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IjmallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IjmallApplication.class, args);
+    }
 
 }
